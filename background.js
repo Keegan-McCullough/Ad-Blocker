89fe980dc39362c7c22be239827025a069012951
Keegan-McCullough <keeganmcculloughfury@gmail.com>
2025-06-03 12:47:02 -0400

    commented out debug listener

--- background.js
+++ background.js
@@ -1,22 +1,20 @@
 let blockedCount = 0;
 let blockedUrls = [];
 
-// Listen for when a rule is matched (ad blocked)
+/* Listen for when a rule is matched (ad blocked)
 chrome.declarativeNetRequest.onRuleMatchedDebug.addListener((info) => {
   blockedCount++;
   blockedUrls.push(info.request.url);
   console.log(`Blocked: ${info.request.url}`);
-});
+});*/
 
 // Respond to messages from popup.js
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === 'getBlockedInfo') {
     sendResponse({
-      count: blockedCount,
-      urls: blockedUrls
+      count: "Unavailable in public extensions",
+      urls: []
     });
   }
   return true; // Needed for async response
 });
-
-  
