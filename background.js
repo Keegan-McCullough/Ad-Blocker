cdf8a2636cfee8ffe9d003378de073d4f626d89f
Keegan-McCullough <keeganmcculloughfury@gmail.com>
2025-06-03 12:48:13 -0400

    got rid of comments

--- background.js
+++ background.js
@@ -1,13 +1,6 @@
 let blockedCount = 0;
 let blockedUrls = [];
 
-/* Listen for when a rule is matched (ad blocked)
-chrome.declarativeNetRequest.onRuleMatchedDebug.addListener((info) => {
-  blockedCount++;
-  blockedUrls.push(info.request.url);
-  console.log(`Blocked: ${info.request.url}`);
-});*/
-
 // Respond to messages from popup.js
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === 'getBlockedInfo') {
