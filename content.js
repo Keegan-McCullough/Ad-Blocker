e3f798dd6573cf7ac07298ee8cace0545d33c7c4
Keegan-McCullough <keeganmcculloughfury@gmail.com>
2025-06-02 22:04:40 -0400

    first part of the project skeleton

---
+++ content.js
@@ -0,0 +1,8 @@
+// Remove elements commonly used for ads
+const adSelectors = ['iframe[src*="ads"]', '[id^="ad-"]', '.ad', '.adsbygoogle'];
+
+window.addEventListener('load', () => {
+  adSelectors.forEach(selector => {
+    document.querySelectorAll(selector).forEach(el => el.remove());
+  });
+});
