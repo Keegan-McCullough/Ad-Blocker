3132a00541c6639094f3dcb6bfde94a08e30af5e
Keegan-McCullough <keeganmcculloughfury@gmail.com>
2025-06-03 12:10:15 -0400

    excluded javascript from html

--- content.js
+++ content.js
@@ -1,5 +1,6 @@
 // Remove elements commonly used for ads
 const adSelectors = ['iframe[src*="ads"]', '[id^="ad-"]', '.ad', '.adsbygoogle'];
+console.log("Ad Blocker script is running.");
 
 window.addEventListener('load', () => {
   adSelectors.forEach(selector => {
